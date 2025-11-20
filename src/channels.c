af1a9d5f5787627f8efbfdda999055c43348a8e1
Mason Hall <hallmason17@gmail.com>
2025-11-20 09:59:21 -0500

    add source code comments

--- src/channels.c
+++ src/channels.c
@@ -11,19 +11,42 @@
 #define CH_CLOSED 1 << 0
 #define CH_BOUNDED 1 << 1
 
+/* The main channel type */
 typedef struct channel_t {
+  /* The size of items in the channel */
   size_t item_size;
+
+  /* The number unread items in the channel */
   size_t count;
+
+  /* The number of unread items that can be in the channel at a time */
   size_t capacity;
+
+  /* Pointer to the next slot for the receiver to take data */
   size_t recv_ptr;
+
+  /* Pointer to the next slot for senders to put data */
   size_t send_ptr;
-  void *queue;
-  uint8_t flags;
+
+  /* Condition variable to wake sleeping producer threads */
   pthread_cond_t send_cond;
+
+  /* Condition variable to wake a sleeping consumer thread */
   pthread_cond_t recv_cond;
+
+  /* Mutex for the queue and condition variables */
   pthread_mutex_t mu;
+
+  /* Flags for state management, bounded or unbounded, open or closed */
+  uint8_t flags;
+
+  /* The buffer used by senders and receivers, whose size is item_size *
+   * capacity */
+  void *queue;
 } channel_t;
 
+/* Initialize a channel of size item_size * capacity and return a pointer to it
+ */
 channel_t *channel_create(size_t item_size, size_t capacity) {
   channel_t *ch = malloc(sizeof(channel_t));
 
@@ -52,6 +75,7 @@ channel_t *channel_create(size_t item_size, size_t capacity) {
   return ch;
 }
 
+/* Send a pointer to value into the channel, place it into the queue */
 bool channel_send(channel_t *ch, const void *value) {
   pthread_mutex_lock(&ch->mu);
   if (ch->flags & CH_CLOSED) {
@@ -68,6 +92,7 @@ bool channel_send(channel_t *ch, const void *value) {
       return false;
     }
   } else if (ch->capacity <= ch->count) {
+    /* Out of room in an unbounded channel, need to increase the size */
     size_t new_cap = ch->capacity * 2;
     void *new_queue = malloc(new_cap * ch->item_size);
     if (new_queue == NULL) {
@@ -75,14 +100,23 @@ bool channel_send(channel_t *ch, const void *value) {
       return false;
     }
     if (ch->recv_ptr < ch->send_ptr) {
+      /* The queue is in the correct order */
       memcpy(new_queue, (char *)ch->queue + ch->recv_ptr * ch->item_size,
              ch->count * ch->item_size);
     } else {
-      size_t start = ch->capacity - ch->recv_ptr;
+      /* If we have wrapped around the end of the queue, need to reorganize */
+      /* Ex. [0.. send_ptr.. recv_ptr.. capacity] */
+
+      /* This grabs [recv_ptr.. capacity], puts it into the new_queue */
+      size_t start_items = ch->capacity - ch->recv_ptr;
       memcpy(new_queue, (char *)ch->queue + ch->recv_ptr * ch->item_size,
-             start * ch->item_size);
-      memcpy((char *)new_queue + start * ch->item_size, ch->queue,
+             start_items * ch->item_size);
+
+      /* Grab the rest and put it after */
+      memcpy((char *)new_queue + start_items * ch->item_size, ch->queue,
              ch->send_ptr * ch->item_size);
+
+      /* New buffer is now properly ordered! */
     }
 
     free(ch->queue);
@@ -91,39 +125,62 @@ bool channel_send(channel_t *ch, const void *value) {
     ch->recv_ptr = 0;
     ch->send_ptr = ch->count;
   }
+
+  /* Copy the value into the correct place in the buffer */
   void *slot = (char *)ch->queue + (ch->item_size * ch->send_ptr);
   memcpy(slot, value, ch->item_size);
   ch->count++;
+
+  /* Buffer is circular for simplicity */
   ch->send_ptr = (ch->send_ptr + 1) % ch->capacity;
+
+  /* Wake up the receiver if it is waiting */
   pthread_cond_signal(&ch->recv_cond);
   pthread_mutex_unlock(&ch->mu);
   return true;
 }
+
+/* Receive an item from the channel if available, write the data into *value */
 bool channel_recv(channel_t *ch, void *value) {
   pthread_mutex_lock(&ch->mu);
+
+  /* Go to sleep if there is nothing in the queue */
   while (ch->count == 0 && !(ch->flags & CH_CLOSED)) {
     pthread_cond_wait(&ch->recv_cond, &ch->mu);
   }
+
+  /* Exit if the channel is closed and empty */
   if (ch->count == 0 && (ch->flags & CH_CLOSED)) {
     pthread_mutex_unlock(&ch->mu);
     return false;
   }
+
+  /* Copy the next item to be received into *value */
   void *slot = (char *)ch->queue + (ch->item_size * ch->recv_ptr);
   memcpy(value, slot, ch->item_size);
   ch->count--;
+
+  /* Buffer is circular for simplicity */
   ch->recv_ptr = (ch->recv_ptr + 1) % ch->capacity;
 
+  /* Wake up a producer if it is waiting for room in the buffer */
   pthread_cond_signal(&ch->send_cond);
   pthread_mutex_unlock(&ch->mu);
   return true;
 }
+
+/* Close the channel off to further sending */
 void channel_close(channel_t *ch) {
   pthread_mutex_lock(&ch->mu);
+
+  /* Set the closed bit, wake up all the sleeping threads */
   ch->flags |= CH_CLOSED;
   pthread_cond_broadcast(&ch->send_cond);
   pthread_cond_broadcast(&ch->recv_cond);
   pthread_mutex_unlock(&ch->mu);
 }
+
+/* Cleanup resources */
 void channel_destroy(channel_t *ch) {
   pthread_cond_destroy(&ch->send_cond);
   pthread_cond_destroy(&ch->recv_cond);
